7f5f8963a212560c77f26684ce305a92074d1cc2
Oleksiuk <ooleksiuk.exceedteam@gmail.com>
2021-05-06 17:44:29 +0300

    string to type

--- 2. Data Types/1) string-to-type/index.js
+++ 2. Data Types/1) string-to-type/index.js
@@ -1,6 +1,22 @@
 function stringToType(str) {
-  // Напиши свой код здесь
-};
+  if (str === '') return '';
+  else if (isNaN(Number(str))) {
+    switch (str) {
+      case 'null':
+        return null;
+      case 'NaN':
+        return NaN;
+      case 'undefined':
+        return undefined;
+      case 'false':
+        return false;
+      case 'true':
+        return true;
+      default:
+        return str;
+    }
+  } else return Number(str) === null ? null : Number(str);
+}
 
 window.stringToType = stringToType;
 
