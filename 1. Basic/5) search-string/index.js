40f5076e4d8cc263d0838ce556d5f98f5d44c8dd
Oleksiuk <ooleksiuk.exceedteam@gmail.com>
2021-05-06 16:06:39 +0300

    fix search-string

--- 1. Basic/5) search-string/index.js
+++ 1. Basic/5) search-string/index.js
@@ -1,12 +1,15 @@
 function searchString(obj) {
   let qs = [];
-  if (!Object.keys(obj).length) return '';
   for (let key in obj) {
     if (obj[key] !== undefined && obj[key] !== null && obj[key] !== '') {
       qs.push(`${key}=${obj[key]}`);
     }
   }
-  return '?' + qs.join('&');
+  if (!qs.length) {
+    return '';
+  } else {
+    return '?' + qs.join('&');
+  }
 }
 
 window.searchString = searchString;
