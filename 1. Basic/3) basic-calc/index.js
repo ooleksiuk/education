e11d59ba8f20632d512bd87ef47ebc36585604c4
Oleksiuk <ooleksiuk.exceedteam@gmail.com>
2021-05-06 12:57:00 +0300

    basic-calc

--- 1. Basic/3) basic-calc/index.js
+++ 1. Basic/3) basic-calc/index.js
@@ -1,5 +1,17 @@
 function basicCalc(operation, a, b) {
-  // Напиши свой код здесь
+  switch (operation) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '/':
+      return a / b;
+    case '*':
+      return a * b;
+    default:
+      console.log('Неверный оператор!');
+      break;
+  }
 }
 
 window.basicCalc = basicCalc;
