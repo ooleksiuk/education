5d0f7192b2d9cbfeeedcb5dfe004ff2889cbb0e6
Oleksiuk <ooleksiuk.exceedteam@gmail.com>
2021-05-07 15:39:34 +0300

    full sum

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,5 +1,17 @@
 function fullSum(...args) {
-  // Напиши свой код здесь
+  // check whether parameters were passed
+  if (!args.length) return 0;
+
+  // check whether all passed parameters are numbers
+  if (
+    args.every((e) => {
+      return typeof e === 'number';
+    })
+  ) {
+    return args.reduce((prev, curr) => prev + curr);
+  } else {
+    throw new Error('Wrong Argument Type');
+  }
 }
 
 window.fullSum = fullSum;
