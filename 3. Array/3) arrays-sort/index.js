b6d221c3eacc14c6dfb8b00dd946fb17b9f978f0
Oleksiuk <ooleksiuk.exceedteam@gmail.com>
2021-05-07 17:08:05 +0300

    arrays sort

--- 3. Array/3) arrays-sort/index.js
+++ 3. Array/3) arrays-sort/index.js
@@ -1,6 +1,13 @@
 function arraysSort(arr) {
-  // Напиши свой код здесь
-};
+  // make copy of the array
+  let sortedArr = arr.slice();
+  sortedArr.sort((a, b) => arrayMax(a) - arrayMax(b));
+  return sortedArr;
+}
+
+function arrayMax(array) {
+  return array.reduce((a, b) => Math.max(a, b));
+}
 
 window.arraysSort = arraysSort;
 
