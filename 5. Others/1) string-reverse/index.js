98d83333159e802705eccefdb6170d8daf16fd3e
Oleksiuk <ooleksiuk.exceedteam@gmail.com>
2021-05-11 14:47:41 +0300

    string-reverse

--- 5. Others/1) string-reverse/index.js
+++ 5. Others/1) string-reverse/index.js
@@ -6,3 +6,13 @@
   'bob'.reverse() === 'bob';
   'abc123'.reverse() === '321cba';
  */
+function reverseString(str) {
+  if (!str) {
+    str = this;
+  }
+
+  return str.split('').reverse().join('');
+}
+
+// extending the JS String Prototype
+String.prototype.reverse = reverseString;
