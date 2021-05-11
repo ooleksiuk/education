7a3c4479b35a7ff15d98b5fe68472a18cb6cbded
Oleksiuk <ooleksiuk.exceedteam@gmail.com>
2021-05-11 15:38:43 +0300

    xhr-get

--- 4. Async/2) xhr-get/index.js
+++ 4. Async/2) xhr-get/index.js
@@ -1,5 +1,20 @@
 function xhrGet(url) {
-  // Напиши свой код здесь
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.responseType = 'json';
+    xhr.send();
+
+    xhr.onload = function () {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject(xhr.response);
+      }
+    };
+  });
 }
 
 window.xhrGet = xhrGet;
