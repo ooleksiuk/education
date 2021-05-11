727f286f0b3434d62201e00adc10fc451ce12b66
Oleksiuk <ooleksiuk.exceedteam@gmail.com>
2021-05-11 10:25:14 +0300

    query to object

--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -1,6 +1,38 @@
 function queryToObject(query) {
-  // Напиши свой код здесь
-};
+  let obj = {};
+  if (query === '' || query === '?') return obj;
+
+  query
+    .slice(1)
+    .split('&')
+    .forEach((e) => addProp(e, obj));
+  return obj;
+}
+
+function addProp(prop, obj) {
+  const [key, value] = prop.split('=');
+  obj[key] = stringToType(value);
+}
+
+function stringToType(str) {
+  if (str === '') return '';
+  else if (isNaN(Number(str))) {
+    switch (str) {
+      case 'null':
+        return null;
+      case 'NaN':
+        return NaN;
+      case 'undefined':
+        return undefined;
+      case 'false':
+        return false;
+      case 'true':
+        return true;
+      default:
+        return str;
+    }
+  } else return Number(str);
+}
 
 window.queryToObject = queryToObject;
 
